dc98215ab3515dc1834fcc9f1b7c43cd5885b675
kagerwol <Wolfgang.Kager@primetals.com>
2026-02-02 19:28:07 +0100

    Commit

--- CheckSunRiseSet.cpp
+++ CheckSunRiseSet.cpp
@@ -79,46 +79,46 @@ int main(int argc, char* argv[], char* envp[])
                 unsigned int Exam_yyyy = 0;                     // This is the Year to become examimed
                 unsigned int Exam_mm = 0;                       // dito month
                 unsigned int Exam_dd = 0;						// dito day 
-				unsigned int ActYear = 0;                       // This is the Year of the actual day to be treated
-				unsigned int ActMonth = 0;  			        // dito month
-				unsigned int ActDay = 0;    		            // dito day
-				unsigned long long sunrise_unix_ms = 0;         // This is the Sunrise in ms after 1970-01-01 00:00:00
-				unsigned long long sunset_unix_ms = 0;          // This is the Sunset in ms after 1970-01-01 00:00:00
-				unsigned long long day_length_ms = 0;           // This is the length of the day in ms
-				size_t theEvalIndex = 0;                        // This is the index of the day to be examined in the vector of sunRiseSetDatas
-				int noArg = 0;								    // This is the number of arguments read by sscanf_s, to check if the line is correct
+				        unsigned int ActYear = 0;                       // This is the Year of the actual day to be treated
+				        unsigned int ActMonth = 0;  			        // dito month
+				        unsigned int ActDay = 0;    		            // dito day
+				        unsigned long long sunrise_unix_ms = 0;         // This is the Sunrise in ms after 1970-01-01 00:00:00
+				        unsigned long long sunset_unix_ms = 0;          // This is the Sunset in ms after 1970-01-01 00:00:00
+				        unsigned long long day_length_ms = 0;           // This is the length of the day in ms
+				        size_t theEvalIndex = 0;                        // This is the index of the day to be examined in the vector of sunRiseSetDatas
+				        int noArg = 0;								    // This is the number of arguments read by sscanf_s, to check if the line is correct
                 char textBuffer[MAX_PATH + 128];
 
-				// Call the Python script to get the sunrise and sunset times
+				        // Call the Python script to get the sunrise and sunset times
                 sprintf_s(textBuffer, sizeof(textBuffer), "py %s %d-%d-%d", tempFile.c_str(), yyyy, mm, dd);
-				std::istringstream sres(cmdPipeExec(textBuffer));   // The output of the Python Program is a long String, which we have to split into lines
-				std::string aSingleLine;							// Here we store the single line of the output  
+				        std::istringstream sres(cmdPipeExec(textBuffer));   // The output of the Python Program is a long String, which we have to split into lines
+				        std::string aSingleLine;							// Here we store the single line of the output  
 
-				// Treat all the Lines we find in the output of the Python Programm
+				        // Treat all the Lines we find in the output of the Python Programm
                 for (size_t lineCnt = 0; (std::getline(sres, aSingleLine)); lineCnt++)
                 {
-					// The first line contains the Day to be examined
+					          // The first line contains the Day to be examined
                     if (lineCnt == 0)
                     {
-						noArg = sscanf_s(aSingleLine.c_str(), "%d-%d-%d", &Exam_yyyy, &Exam_mm, &Exam_dd);
+						            noArg = sscanf_s(aSingleLine.c_str(), "%d-%d-%d", &Exam_yyyy, &Exam_mm, &Exam_dd);
                         if (noArg != 3)
                         {
-							std::cerr << "Python Program written back wrong Examinated Date:" << aSingleLine << std::endl;
-							throw std::runtime_error("");
+							            std::cerr << "Python Program written back wrong Examinated Date:" << aSingleLine << std::endl;
+							            throw std::runtime_error("");
                         }
                         continue;
                     }
 
-					// All other Lines should contain the Data for the Day: The Date itself, the Sunrise, the Sunset and the Sunlight length
-					noArg = sscanf_s(aSingleLine.c_str(), "%d-%d-%d, %llu, %llu, %llu", &ActYear, &ActMonth, &ActDay, &sunrise_unix_ms, &sunset_unix_ms, &day_length_ms);
+					          // All other Lines should contain the Data for the Day: The Date itself, the Sunrise, the Sunset and the Sunlight length
+					          noArg = sscanf_s(aSingleLine.c_str(), "%d-%d-%d, %llu, %llu, %llu", &ActYear, &ActMonth, &ActDay, &sunrise_unix_ms, &sunset_unix_ms, &day_length_ms);
                     if (noArg != 6)
                     {
                         std::cerr << "Python Programm written back wrong response in line " << lineCnt << ": " << aSingleLine << std::endl;
                         throw std::runtime_error("");
                     }
 
-					// Add the tupel to the vector of sunRiseSetDatas
-					sunRiseSetDatas.addDayData(Exam_yyyy, Exam_mm, Exam_dd, ActYear, ActMonth, ActDay, sunrise_unix_ms, sunset_unix_ms, day_length_ms);
+					          // Add the tupel to the vector of sunRiseSetDatas
+					          sunRiseSetDatas.addDayData(Exam_yyyy, Exam_mm, Exam_dd, ActYear, ActMonth, ActDay, sunrise_unix_ms, sunset_unix_ms, day_length_ms);
 
                     // Process each line here
                     // std::cout << "Processing line: " << aSingleLine << std::endl;
